4b236ec8dc002bc912e360ae53a77f3cb9f6d279
ajboloor <adith94@gmail.com>
2018-05-05 14:14:13 -0500

    converted helloworld to function

--- simple-helloworld/simple-helloworld.rb
+++ simple-helloworld/simple-helloworld.rb
@@ -1,18 +1,22 @@
-puts "HelloWorld!"
-puts "\n"
+def HelloWorld
+  puts "HelloWorld!"
+  puts "\n"
 
-puts "What is your name?"
-name = gets.chomp
-puts "Your name is #{name}.\n"
-puts "\n"
+  puts "What is your name?"
+  name = gets.chomp
+  puts "Your name is #{name}.\n"
+  puts "\n"
 
-a = 10
-b = 7
+  a = 10
+  b = 7
 
-puts "a = " + a.to_s
-puts "b = " + b.to_s
-puts "a + b = " + (a + b).to_s
-puts "a - b = " + (a - b).to_s
-puts "a * b = " + (a * b).to_s
-puts "a / b = " + (a.to_f / b).to_s
-puts "a ^ b = " + (a ** b).to_s
+  puts "a = " + a.to_s
+  puts "b = " + b.to_s
+  puts "a + b = " + (a + b).to_s
+  puts "a - b = " + (a - b).to_s
+  puts "a * b = " + (a * b).to_s
+  puts "a / b = " + (a.to_f / b).to_s
+  puts "a ^ b = " + (a ** b).to_s
+end
+
+HelloWorld()
