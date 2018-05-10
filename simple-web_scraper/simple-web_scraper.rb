90e77c54be7afda337d050ab081964b08f0fc110
ajboloor <adith94@gmail.com>
2018-05-09 22:28:33 -0500

    added method for web_scraper

--- simple-web_scraper/simple-web_scraper.rb
+++ simple-web_scraper/simple-web_scraper.rb
@@ -1,9 +1,14 @@
 require 'nokogiri'   
 require 'open-uri'
 
-page_url = "https://old.reddit.com"
-page = Nokogiri::HTML(open(page_url))
+def web_scraper(page_url, partial_html)
+  page = Nokogiri::HTML(open(page_url))
 
-for posts in page.css('p[class="title"]') do
-   puts posts.text
-end 
+  for posts in page.css(partial_html) do
+     puts posts.text
+  end 
+end
+
+url = "https://old.reddit.com"
+scrap_html = 'p[class="title"]'
+web_scraper(url, scrap_html)
